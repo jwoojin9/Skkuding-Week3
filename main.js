1ed5410f4d534776f3e4fa21319868b740c0820e
jwoojin9 <jwoojin9@naver.com>
2024-04-03 12:10:51 +0900

    asd

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
-  for (var i=1;i<=data.length;i++) {
+  for (let i=1;i<=data.length;i++) {
     const pokemon = data[i-1];
-    var a = document.createElement("a");
+    const a = document.createElement("a");
     a.className += "card";
     a.href = `./pokemon/index.html?n=${i-1}`;
-    a.innerHTML = `<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${i}.png" alt="${pokemon.name}">
+    a.insertAdjacentHTML('afterbegin', 
+    `<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${i}.png" alt="${pokemon.name}">
     <div class="info-wrapper">
       <h2>${pokemon.name}</h2>
       <p>Height: ${pokemon.height}</p>
       <p>Weight: ${pokemon.weight}</p>
       <p>Types: ${pokemon.types.join(", ")}</p>
-    </div>`;
+    </div>
+    `);
     document.getElementById("card-wrapper").appendChild(a);
   }
--- pokemon/main.js
+++ pokemon/main.js
@@ -2,24 +2,24 @@ const urlParams = new URL(location.href).searchParams;
 const n = parseInt(urlParams.get('n'));
 history.replaceState({}, null, location.pathname);
 
-var img = document.createElement("img");
+const img = document.createElement("img");
 img.src=`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${n+1}.png`
 img.alt = data[n].name;
 document.querySelector(".img-wrapper").appendChild(img);
 
-var h2 = document.createElement("h2");
+const h2 = document.createElement("h2");
 h2.innerText = data[n].name;
 document.querySelector("main").appendChild(h2);
 
-var table = document.createElement("table");
-var tbody = document.createElement("tbody");
+const table = document.createElement("table");
+const tbody = document.createElement("tbody");
 table.appendChild(tbody)
 for (const key in data[n]) {
-    var tr = document.createElement("tr");
-    tr.innerHTML = `
-    <td>${key}</td>
-    <td>${data[n][key]}</td>
-    `
-    tbody.appendChild(tr);
-  }
+  const tr = document.createElement("tr");
+  tr.insertAdjacentHTML('afterbegin', `
+  <td>${key}</td>
+  <td>${data[n][key]}</td>
+  `);
+  tbody.appendChild(tr);
+}
 document.querySelector("main").appendChild(table);
